a7a474569f9979cefc2cef5f2e46b0ef96c4c620
Wellington Dias <wsilva187@gmail.com>
2021-09-08 19:34:13 -0300

    Domain model, database seed

---
+++ backend/src/main/java/com/devsuperior/dsvendas/entities/Sale.java
@@ -0,0 +1,35 @@
+package com.devsuperior.dsvendas.entities;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tb_sales")
+public class Sale {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Integer visited;
+    private Integer deals;
+    private Double amount;
+    private LocalDate date;
+
+    @ManyToOne
+    @JoinColumn(name = "seller_id")
+    private Seller seller;
+}
---
+++ backend/src/main/java/com/devsuperior/dsvendas/entities/Seller.java
@@ -0,0 +1,32 @@
+package com.devsuperior.dsvendas.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tb_sellers")
+public class Seller {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "seller")
+    private List<Sale> sales =  new ArrayList<>();
+    
+}
